5edda9857b1d7d69a568c15c3edc744d0067f13d
mfukechristian <mfukechristian@gmail.com>
2025-04-09 16:11:18 +0200

    initialize project with Express server and dotenv configuration

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
